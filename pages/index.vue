fa743bd7ee96466ba8b348fcc47852ca3ececc24
Sergio Luiz Dobri <sergiodobri@gmail.com>
2021-03-11 22:26:55 -0300

    seed products

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">nuxt-express</h1>
-      <div>
-        {{ status }}
-      </div>
+      {{ status }}
     </div>
   </div>
 </template>
@@ -14,42 +10,321 @@
 export default {
   async asyncData({ $http }) {
     const res = await $http.$get('/api/users/999')
-    // const res = await $http.$post('/api/users', { username: 'sergio', password: 'xxx' })
-    console.log('status: ', res.ok) // eslint-disable-line
     return { status: res }
+  },
+
+  data() {
+    return {
+      products: [
+        {
+          show: true,
+          name: 'Asa de Frango',
+          unit: 'Kilo',
+          amount: 18.99
+        },
+        {
+          show: true,
+          name: 'Alcatra',
+          unit: 'Kilo',
+          amount: 35.99
+        },
+        {
+          show: true,
+          name: 'Bife Especial',
+          unit: 'Kilo',
+          amount: 29.99
+        },
+        {
+          show: true,
+          name: 'Bife Mole',
+          unit: 'Kilo',
+          amount: 30.99
+        },
+        {
+          show: true,
+          name: 'Bisteca Lombo',
+          unit: 'Kilo',
+          amount: 18.99
+        },
+        {
+          show: true,
+          name: 'Bisteca Copa',
+          unit: 'Kilo',
+          amount: 17.99
+        },
+        {
+          show: true,
+          name: 'Bucho',
+          unit: 'Kilo',
+          amount: 16.99
+        },
+        {
+          show: true,
+          name: 'Capa do Contra Filé',
+          unit: 'Kilo',
+          amount: 27.99
+        },
+        {
+          show: true,
+          name: 'Chuleta do Contra Filé',
+          unit: 'Kilo',
+          amount: 33.99
+        },
+        {
+          show: true,
+          name: 'Chuleta Gaúcha',
+          unit: 'Kilo',
+          amount: 23.49
+        },
+        {
+          show: true,
+          name: 'Chuletinha',
+          unit: 'Kilo',
+          amount: 24.99
+        },
+        {
+          show: true,
+          name: 'Contra Filé',
+          unit: 'Kilo',
+          amount: 38.99
+        },
+        {
+          show: true,
+          name: 'Costela Minguinha',
+          unit: 'Kilo',
+          amount: 20.99
+        },
+        {
+          show: true,
+          name: 'Costela Suina',
+          unit: 'Kilo',
+          amount: 20.99
+        },
+        {
+          show: true,
+          name: 'Costela Paulista',
+          unit: 'Kilo',
+          amount: 22.99
+        },
+        {
+          show: true,
+          name: 'Costelão',
+          unit: 'Kilo',
+          amount: 21.99
+        },
+        {
+          show: true,
+          name: 'Coxa de Frango',
+          unit: 'Kilo',
+          amount: 8.49
+        },
+        {
+          show: true,
+          name: 'Coxão Duro',
+          unit: 'Kilo',
+          amount: 30.99
+        },
+        {
+          show: true,
+          name: 'Coxão Mole',
+          unit: 'Kilo',
+          amount: 31.49
+        },
+        {
+          show: true,
+          name: 'Coxinha da Asa',
+          unit: 'Kilo',
+          amount: 15.99
+        },
+        {
+          show: true,
+          name: 'Cupin',
+          unit: 'Kilo',
+          amount: 29.99
+        },
+        {
+          show: true,
+          name: 'Filé de Frango',
+          unit: 'Kilo',
+          amount: 13.99
+        },
+        {
+          show: true,
+          name: 'Filé Mignom',
+          unit: 'Kilo',
+          amount: 38.99
+        },
+        {
+          show: true,
+          name: 'Fraldinha',
+          unit: 'Kilo',
+          amount: 31.49
+        },
+        {
+          show: true,
+          name: 'Frango Inteiro',
+          unit: 'Peça',
+          amount: 7.49
+        },
+        {
+          show: true,
+          name: 'Frango a Passarinho',
+          unit: 'Kilo',
+          amount: 8.99
+        },
+        {
+          show: true,
+          name: 'Figado',
+          unit: 'Kilo',
+          amount: 13.99
+        },
+        {
+          show: true,
+          name: 'Kibe crú',
+          unit: 'Kilo',
+          amount: 21.99
+        },
+        {
+          show: true,
+          name: 'Linguiça Mista',
+          unit: 'Kilo',
+          amount: 11.99
+        },
+        {
+          show: true,
+          name: 'Linguiça Cuiabana',
+          unit: 'Kilo',
+          amount: 19.99
+        },
+        {
+          show: true,
+          name: 'Linguiça Toscana',
+          unit: 'Kilo',
+          amount: 15.99
+        },
+        {
+          show: true,
+          name: 'Miolo da Paleta',
+          unit: 'Kilo',
+          amount: 29.99
+        },
+        {
+          show: true,
+          name: 'Miolo de Acém',
+          unit: 'Kilo',
+          amount: 27.99
+        },
+        {
+          show: true,
+          name: 'Mocotó',
+          unit: 'Kilo',
+          amount: 10.95
+        },
+        {
+          show: true,
+          name: 'Músculo',
+          unit: 'Kilo',
+          amount: 26.99
+        },
+        {
+          show: true,
+          name: 'Ossobuco',
+          unit: 'Kilo',
+          amount: 22.99
+        },
+        {
+          show: true,
+          name: 'Panceta',
+          unit: 'Kilo',
+          amount: 22.99
+        },
+        {
+          show: true,
+          name: 'Patinho',
+          unit: 'Kilo',
+          amount: 30.99
+        },
+        {
+          show: true,
+          name: 'Pernil Fatiado',
+          unit: 'Kilo',
+          amount: 16.99
+        },
+        {
+          show: true,
+          name: 'Pernil Picado',
+          unit: 'Kilo',
+          amount: 14.99
+        },
+        {
+          show: true,
+          name: 'Picadão bovino',
+          unit: 'Kilo',
+          amount: 26.99
+        },
+        {
+          show: true,
+          name: 'Picadão suino',
+          unit: 'Kilo',
+          amount: 14.99
+        },
+        {
+          show: true,
+          name: 'Picanha',
+          unit: 'Kilo',
+          amount: 54.99
+        },
+        {
+          show: true,
+          name: 'Ponta de Agulha',
+          unit: 'Kilo',
+          amount: 19.99
+        },
+        {
+          show: true,
+          name: 'Rabada',
+          unit: 'Kilo',
+          amount: 22.99
+        },
+        {
+          show: true,
+          name: 'Tirinhas Strogonoff',
+          unit: 'Kilo',
+          amount: 27.99
+        },
+        {
+          show: false,
+          name: 'Toscana do Shopping',
+          unit: 'Kilo',
+          amount: 12.99
+        },
+        {
+          show: true,
+          name: 'Tosana com Queijo',
+          unit: 'Kilo',
+          amount: 17.99
+        },
+        {
+          show: true,
+          name: 'Toucinho',
+          unit: 'Kilo',
+          amount: 14.99
+        },
+        {
+          show: true,
+          name: 'Tulipa temperada',
+          unit: 'Kilo',
+          amount: 27.99
+        }
+      ]
+    }
+  },
+
+  created() {
+    this.products.forEach((product) => {
+      const xx = this.$http.$post('/api/products', product)
+      console.log(xx)
+    })
   }
 }
 </script>
-
-<style scoped>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue',
-    Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
